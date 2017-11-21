706504693fdc63c6bc1faa3926601b8e61d35633
Marsch Huynh <marsch.huynh@gmail.com>
2017-11-21 20:43:42 +0700

    Support python3

--- face-detector.py
+++ face-detector.py
@@ -16,14 +16,14 @@ while (True):
 
 	faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 
-	print faces
-	for (x,y,w,h) in faces:
-		cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+	print(faces)
+	for (x, y, w, h) in faces:
+		cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
 		roi_gray = gray[y:y+h, x:x+w]
 		roi_color = img[y:y+h, x:x+w]
 		eyes = eye_cascade.detectMultiScale(roi_gray)
 		for (ex,ey,ew,eh) in eyes:
-			cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+			cv2.rectangle(roi_color, (ex, ey),(ex+ew,ey+eh),(0,255,0),2)
 	cv2.imshow('img',img)
 	key = cv2.waitKey(20)
 	if key == 27: # exit on ESC
@@ -32,5 +32,3 @@ while (True):
 # cv2.imshow('img',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
